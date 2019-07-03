82beac8a87b65f87eb731d4c21481f9d715aaac7
MrZhang <332004976@qq.com>
2019-07-03 12:11:42 +0800

    day3

---
+++ day3/react-project/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import 'weui';
+import Header from './components/Header/Header'
+import Search from './components/Search/Search'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Search/>
+    </div>
+  );
+}
+
+export default App;
